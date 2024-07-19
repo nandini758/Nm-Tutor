431e0d4487130bcbfb2b21b0c9b80518ef8c0ec1
Nandini <nandinimehta758@gmail.com>
2024-07-19 20:59:38 +0530

    project-setup

---
+++ src/MyComponent/SecondComponent.jsx
@@ -0,0 +1,49 @@
+function SecondComponent(){
+    return (
+        <div className="flex flex-col justify-center text-5xl font-bold text-center text-white bg-green-500 leading-[54.9px] max-md:text-4xl">
+          <div className="flex overflow-hidden relative flex-col items-center px-16 pt-16 pb-11 w-full min-h-[145px] max-md:px-5 max-md:max-w-full max-md:text-4xl">
+            What will be Covered?
+          </div>
+          <div className="flex overflow-hidden relative flex-col justify-center items-center px-16 py-5 w-full min-h-[296px] max-md:px-5 max-md:max-w-full">
+        <div className="relative max-w-full w-[1120px]">
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+            <div className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+              <div className="flex overflow-hidden relative flex-col grow items-center px-20 pt-4 pb-10 text-base leading-7 text-center text-white capitalize rounded-xl border-r-4 border-b-4 border-green-600 border-solid shadow-sm aspect-[1.41] max-md:px-5 max-md:mt-5">
+                <img
+                  loading="lazy"
+                  srcSet="https://cdn-ilajebb.nitrocdn.com/wGFtqfgKtokGEDWasoQhcaTkMaHhbUSN/assets/images/optimized/rev-7f9434c/therahulawasthi.com/wp-content/uploads/2024/05/1.png"
+                  className="mx-3 w-44 aspect-[1.08]"
+                />
+                <div className="relative mt-6">complete mentorship</div>
+              </div>
+            </div>
+            <div style={{borderRadius:"13px"}} className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full bg-black ">
+              <div className="flex overflow-hidden relative flex-col grow px-20 pt-4 pb-10 text-base leading-7 text-center text-white capitalize rounded-xl border-r-4 border-b-4 border-green-600 border-solid shadow-sm aspect-[1.41] max-md:px-5 max-md:mt-5">
+                <img
+                  loading="lazy"
+                  srcSet="https://cdn-ilajebb.nitrocdn.com/wGFtqfgKtokGEDWasoQhcaTkMaHhbUSN/assets/images/optimized/rev-7f9434c/therahulawasthi.com/wp-content/uploads/2024/05/2.png"
+                  className="self-center max-w-full aspect-square w-[163px]"
+                />
+                <div className="relative mt-6">Course Curriculum Design</div>
+              </div>
+            </div>
+            <div className="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+              <div className="flex overflow-hidden relative flex-col grow px-14 pt-4 pb-10 text-base leading-7 text-center text-white capitalize rounded-xl border-r-4 border-b-4 border-green-600 border-solid shadow-sm aspect-[1.41] max-md:px-5 max-md:mt-5">
+                <img
+                  loading="lazy"
+                  srcSet="https://cdn-ilajebb.nitrocdn.com/wGFtqfgKtokGEDWasoQhcaTkMaHhbUSN/assets/images/optimized/rev-7f9434c/therahulawasthi.com/wp-content/uploads/2024/05/3-1.png"
+                  className="self-center max-w-full aspect-square w-[163px]"
+                />
+                <div className="relative mt-6">
+                  Profile Building & Optimization
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>
+      );
+}
+
+export default SecondComponent;
